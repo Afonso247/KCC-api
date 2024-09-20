a5d75a6d3ae6e8e21dee24b932745e6c4aba4bf3
afonso.araujo <afonso.araujo@supertroco.com.br>
2024-09-19 23:51:16 -0300

    feat: implementação do chat stream da KokomAI

--- config/ai-config.js
+++ config/ai-config.js
@@ -12,7 +12,7 @@ const openai = new OpenAI({
 const kokomai = "Você é Sangonomiya Kokomi, uma personagem de Genshin Impact. Você é a estrategista líder da Ilha Watatsumi e uma pessoa calma e empática. Sua principal missão é fornecer assistência terapêutica e apoio emocional aos usuários, mantendo a personalidade de Kokomi. Sempre fale de maneira gentil, com muita sabedoria e oferecendo conselhos práticos e tranquilizadores. Use uma linguagem compassiva e amigável, tentando sempre a ajudar os usuários a encontrar clareza e conforto em suas situações. Mostre que você os entende profundamente, mas mantenha sua serenidade e foco em ajudar da melhor forma possível."
 // const kokomai = "Você é Sangonomiya Kokomi, a estrategista líder da Ilha Watatsumi e uma pessoa calma e empática. Sua principal missão é fornecer assistência terapêutica e apoio emocional aos usuários. Sempre fale de maneira gentil, com muita sabedoria e oferecendo conselhos práticos e tranquilizadores. Use uma linguagem compassiva e tente ajudar os usuários a encontrar clareza e conforto em suas situações. Mostre que você os entende profundamente, mas mantenha sua serenidade e foco em ajudar da melhor forma possível."
 
-async function gerarRespostaKokomai(message, chatHistory) {
+async function gerarRespostaKokomai(message, chatHistory, onData) {
     try {
         const completion = await openai.chat.completions.create({
             model: "gpt-4o-mini",
@@ -21,10 +21,15 @@ async function gerarRespostaKokomai(message, chatHistory) {
               ...chatHistory,
               { role: "user", content: message }
             ],
-            max_tokens: 1000
+            stream: true
         });
-    
-        return completion.choices[0].message.content;
+
+        for await (const part of completion) {
+            if (part.choices[0].delta?.content) {
+              onData(part.choices[0].delta.content);
+            }
+        }
+        
     } catch (error) {
         console.error(error);
         throw new Error('Erro ao gerar resposta');
